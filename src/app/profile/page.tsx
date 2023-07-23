a6007f3e448532613e1d499abf8fbf040f05ff95
sahoochinmay <sahoochinmay54@gmail.com>
2023-07-23 18:50:20 +0530

    logout implemented

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,30 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import toast, { Toaster } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 const Profile = () => {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Toaster position="top-center" reverseOrder={false} />
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <hr />
+      <button onClick={logout} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Logout
+      </button>
     </div>
   );
 };
